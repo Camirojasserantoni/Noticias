05366c856ebc03116e4fe302f0b3323ca323189f
Camirojasserantoni <camilarojasserantoni@gmail.com>
2020-05-11 19:29:57 -0400

    Updates

---
+++ precache-manifest.97d724177433fb57099413ac9556885d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9c3048babcc27f958681906dfe93e54",
+    "url": "/Noticias-react-gh-pages/index.html"
+  },
+  {
+    "revision": "b5afa12a1db823c3c263",
+    "url": "/Noticias-react-gh-pages/static/css/main.2ba0f2f9.chunk.css"
+  },
+  {
+    "revision": "3fd387c46a264ccba0bd",
+    "url": "/Noticias-react-gh-pages/static/js/2.b94288f6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Noticias-react-gh-pages/static/js/2.b94288f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b5afa12a1db823c3c263",
+    "url": "/Noticias-react-gh-pages/static/js/main.88ce46b1.chunk.js"
+  },
+  {
+    "revision": "cd93bcae01eeccbfec9e",
+    "url": "/Noticias-react-gh-pages/static/js/runtime-main.62582b9d.js"
+  }
+]);
